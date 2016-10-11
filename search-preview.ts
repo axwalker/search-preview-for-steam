8d3000af6b2a1cc3817f549c2bf7dbea13409ca1
Andrew Walker <awalkerdev@gmail.com>
2016-10-11 14:49:38 +0100

    feat: show game description in preview

--- search-preview.ts
+++ search-preview.ts
@@ -1,25 +1,17 @@
-declare var WebKitMutationObserver: any;
-
-
 const SEARCH_RESULT_CONTAINER = 'search_result_container';
 
-
 class GameContainer {
-    el: HTMLAnchorElement;
+    constructor(private el: HTMLAnchorElement) {}
 
-    constructor(el) {
-        this.el = el;
-    }
-
-    static appendScreenshotsToAllGames() {
-        let appendScreenshots = () => {
+    static appendPreviewToAllGames(): void {
+        let appendAllPreviews = () => {
             let games = GameContainer.gamesFromPage();
-            games.forEach(g => g.appendScreenshots());
+            games.forEach(g => g.appendPreview());
         };
-        appendScreenshots();
+        appendAllPreviews();
 
         let container = document.getElementById(`${SEARCH_RESULT_CONTAINER}`);
-        let observer = new WebKitMutationObserver(appendScreenshots);
+        let observer = new WebKitMutationObserver(appendAllPreviews);
         observer.observe(container, { childList: true });
         observer.observe(container.parentNode, { childList: true });
     }
@@ -29,38 +21,88 @@ class GameContainer {
         return [].slice.call(anchors).map(el => new GameContainer(el));
     }
 
-    getScreenshots() {
-        return getHtml(this.el.href).then(html => {
-            let container = document.createElement('div');
-            container.innerHTML = html;
-            let images = container.querySelectorAll('.highlight_strip_screenshot > img');
-            return [].slice.call(images).map(img => img.src);
-        });
-    }
-
-    appendScreenshots() {
+    appendPreview() {
         if (this.el.getAttribute('has-gallery')) {
             return;
         }
+        this.el.setAttribute('has-gallery', 'true');
 
-        return this.getScreenshots().then(srcs => {
-            if (!srcs) {
+        return this.getGameDetails().then(details => {
+            if (!details) {
                 return;
             }
-            let images = document.createElement('div');
-            images.style.background = '#2a3f5a';
-            images.style.margin = '10px';
-            images.style.marginBottom = '30px';
-            images.innerHTML = srcs
-                .map(s => `<img src=${s}>`)
-                .join(' ');
-            this.el.parentNode.insertBefore(images, this.el.nextSibling);
-            this.el.setAttribute('has-gallery', 'true');
+            this.el.parentNode.insertBefore(preview(details), this.el.nextSibling);
+        });
+    }
+
+    getGameDetails(): Promise<GameDetails> {
+        return getHtml(this.el.href).then(html => {
+            let container = document.createElement('div');
+            container.innerHTML = html;
+            let descriptionContainer = container.querySelector('.game_description_snippet');
+            if (!descriptionContainer) {
+                return;
+            }
+            let description = descriptionContainer.textContent.trim();
+            let images = container.querySelectorAll('.highlight_strip_screenshot > img');
+            let screenshots = [].slice.call(images).map(img => img.src);
+            return { description, screenshots };
         });
     }
 }
 
+interface GameDetails {
+    description: string;
+    screenshots: string[];
+}
 
+
+/* TEMPLATES */
+const IMAGES_WIDTH = '75%';
+const DESCRIPTION_WIDTH = '21%';
+const BG_COLOR = '#2a3f5a';
+const PADDING = '5px';
+const MARGIN_BETWEEN_GAMES = '30px';
+
+let styles = document.createElement('style');
+styles.innerHTML = `
+    .ssp-container {
+        background-color: ${BG_COLOR};
+        padding: ${PADDING};
+        margin-top: -5px;
+        margin-bottom: ${MARGIN_BETWEEN_GAMES};
+    }
+    .ssp-item {
+        vertical-align: top;
+        padding: 5px;
+        display: inline-block;
+    }
+    .ssp-screenshots {
+        width: ${IMAGES_WIDTH};
+    }
+    .ssp-description {
+        width: ${DESCRIPTION_WIDTH};
+    }
+`;
+document.head.appendChild(styles);
+
+function preview(details: GameDetails) {
+    let preview = document.createElement('div');
+    let screenshots = screenshotImages(details.screenshots);
+    preview.className = 'ssp-container';
+    preview.innerHTML = `
+        <div class="ssp-item ssp-screenshots">${screenshots}</div>
+        <div class="ssp-item ssp-description">${details.description}</div>
+    `;
+    return preview;
+}
+
+function screenshotImages(srcs: string[]): string {
+    return srcs.map(s => `<img src=${s}>`).join(' ');
+}
+
+
+/* UTIL */
 function getHtml(url): Promise<string> {
     return new Promise(resolve => {
         let xmlHttp = new XMLHttpRequest();
@@ -76,4 +118,9 @@ function getHtml(url): Promise<string> {
 }
 
 
-GameContainer.appendScreenshotsToAllGames();
+/* TS DECLARATIONS */
+declare var WebKitMutationObserver: any;
+
+
+/* MAIN */
+GameContainer.appendPreviewToAllGames();
